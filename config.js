77c6026a26dd7971ca72d2b1d7d0ee91a08f645a
duytrieu235 <86992508+duytrieu235@users.noreply.github.com>
2021-07-06 11:43:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,15 +4,15 @@ const CONFIG = {
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'moahh',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích emm <33',
-    btnNo: 'Không, Em gái à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu Nhunggg <3',
-    btnReply: 'Gửi cho em <3',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh traii à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
+    mess: 'Anh biết mà 🥰. Yêu em nhiềuuuu 😘😘',
     messDesc: 'Ngại gì mà không đi inbox cho tuii nhanh đii nè cậu 💚💚💚 ',
     btnAccept: 'Okiiiii lun <3',
     messLink:
